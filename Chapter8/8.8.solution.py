5ae89604566637c64622cffc59e2543cef3cb5a0
yena-yena <ktceng1234@naver.com>
2021-11-22 00:03:12 +0900

    1122

--- Chapter8/8.8.solution.py
+++ Chapter8/8.8.solution.py
@@ -1,4 +1,5 @@
 import random as r
+import re
 
 picklist = list()
 numlist = list()
@@ -8,26 +9,34 @@ for i in range(10):
     pick = r.randint(1, 1000)
     picklist.append(pick)
 
-print(picklist)
-# picklist = list(map(int, picklist))   --->요거 잘못됨 그래서 이중 리스트 씌워진듯
+# print(picklist)
+picklist = list(map(int, picklist))
 
-# print(type(picklist))
 
 
 f = open("random_numbers.txt", "w")
 f.write(str(picklist))
 f.close()
-# print(type(picklist))
 
 # random_numbers 파일 읽기
 fr = open("random_numbers.txt", "r")
-reading = fr.readline()
+temp = fr.readlines()
 
+#,를 기준으로 문자열을 만들기
+s = ",".join(temp)
+#숫자만을 뽑아내서 리스트에 넣기
+s = re.findall("\d+", s)
+# print(s)
+
+#문자열 타입의 리스트를 정수형으로 바꾸기
+picklist = list(map(int,s))
 
 # 짝수 구별하는 코드
-for i in reading:
+for i in picklist:
+    #i = int(i) # i의 type : str
+
     # 문자열 형식 지정 중에 변환되지 않은 인수도 있습니다.
-    if int(i) % 2 == 0:
+    if i % 2 == 0:
         numlist.append(i)
 
     else:
@@ -41,7 +50,7 @@ fw.close()
 
 # 홀수 구별하는 코드
 for i in picklist:
-
+    #i = int(i) # i의 type : str
     # 문자열 형식 지정 중에 변환되지 않은 인수도 있습니다.
     if i % 2 == 0:
         continue
